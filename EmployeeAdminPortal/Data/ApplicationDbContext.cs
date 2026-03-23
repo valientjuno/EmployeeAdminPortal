15076fdffed6957862ac0ec2639c987890049071
valientjuno <jessedoake91@gmail.com>
2026-03-23 12:41:38 -0600

    Add comment to ApplicationDbContext constructor
    Added a comment to the ApplicationDbContext class.

--- EmployeeAdminPortal/Data/ApplicationDbContext.cs
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models;
 using EmployeeAdminPortal.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeAdminPortal.Data
 {
+    // Constructor for the application's database context, inheriting from Entity Framework Core's DbContext
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
